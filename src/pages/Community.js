8eccfbdfdd318e6207b66c684a959c419d2286a4
Jaden Leonard <91337033+jadenl1@users.noreply.github.com>
2022-01-12 16:14:31 -0500

    Update Community.js

--- src/pages/Community.js
+++ src/pages/Community.js
@@ -3,10 +3,21 @@ import NavBar from '../components/NavBar'
 
 const Community = () => {
     return (
-        <div>
+        <>
             <NavBar/>
-            <h1>Community</h1>
-        </div>
+            
+            <div id='forum'>
+                <h1>Community</h1>
+                
+                <h2>Post Text:</h2>
+                <input type="text" />
+                <h2>Username</h2>
+                <input type="text" />
+                <br/>
+                <br/>
+                <button>submit</button>
+            </div>
+        </>
     )
 }
 
