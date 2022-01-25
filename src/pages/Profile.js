76558c2b95a759cc29298fa3312ec26924894539
Jaden Leonard <91337033+jadenl1@users.noreply.github.com>
2022-01-25 02:38:58 -0500

    v.3.0.2 - user + post linkage
    + account now displays user's posts
    
    - glitch in mobile view of community page
    
    adding soon++++++++
    + users can delete their own posts
    + users can create posts directly from profile
    + search bar for own posts? (maybe)

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,6 +1,8 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import { Link, useNavigate } from 'react-router-dom'
+
 import { useAuth } from '../contexts/AuthContext'
+import {db} from '../firebase'
 
 import NavBar from '../components/NavBar'
 import Footer from '../components/Footer'
@@ -17,6 +19,33 @@ export default function Profile(){
     const userName = "@" + (currentUser.email).slice(0, atPos);
     const emailExt = (currentUser.email).slice(atPos, currentUser.length);
 
+    const [posts, setPosts] = useState([]);
+
+    
+    // setPosts(db.collection("posts").where("username", "==", userName).get());
+    // console.log(posts);
+
+    useEffect(() => {
+        const getPostsFromFirebase = [];
+        const subscriber = db
+        .collection('posts')
+        .where("userID", "==", currentUser.uid)
+        // .orderBy('date', "desc")
+        .onSnapshot((querySnapshot) => {
+            querySnapshot.forEach((doc) => {
+                getPostsFromFirebase.push({
+                    ...doc.data(),
+                    key: doc.id,
+                });
+            });
+            setPosts(getPostsFromFirebase);
+        });
+
+        return () => subscriber();
+    }, []);
+
+    console.log(posts);
+
     async function handleLogout(){
 
         setError("");
@@ -32,17 +61,45 @@ export default function Profile(){
     return (
         <>
             <NavBar/>
-            <div id='profile-head'>
-                <p>MY PROFILE</p>
-                <div id = 'full-name'>
-                    <p id = 'name'>{userName}</p>
-                    <p id = 'email-ext'>{emailExt}</p>
+            <div id="profile-page">
+                <div id='profile-head'>
+                    <p>MY PROFILE</p>
+                    <div id = 'full-name'>
+                        <p id = 'name'>{userName}</p>
+                        <p id = 'email-ext'>{emailExt}</p>
+                    </div>
+                    <div id = 'buttons'>
+                        <Link to='/update-profile'><button>update profile</button></Link>
+                        <a><button onClick={handleLogout}>log out</button></a>
+                    </div>
                 </div>
-                <div id = 'buttons'>
-                    <Link to='/update-profile'><button>update profile</button></Link>
-                    <a><button onClick={handleLogout}>log out</button></a>
-                 </div>
+                <div id="posts">
+                    <h2 id="sub-title">your posts&#8628;</h2>
+                    {posts.length > 0 ? (
+                        posts.slice(0,10).map((post) => {
+                            return(
+                                <div id = 'post'>
+                                    
+                                    <h2 key={post.key}>{post.word}</h2>
+                                    <p id = 'pos'>{post.pos}</p>
+
+                                    <h4>definition &#8628;</h4>
+                                    <p id = 'definition'>{post.definition}</p> 
+
+                                    <div id = 'post-footer'>
+                                        <p id = 'num-likes'>{post.numLikes} likes</p>
+                                        <p id = 'username'>{post.username}</p>
+                                    </div>
+                                
+                                </div>
+                            );
+                        })
+                    ) : (
+                        <p></p>
+                    )}
+                </div>
             </div>
+
             <Footer/>
         </>
     )
